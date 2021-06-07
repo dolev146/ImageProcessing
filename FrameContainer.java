a6a9b9d08de7a7e653d77fa03519eef5570443d8
dolev146 <dolev146@gmail.com>
2021-06-07 21:26:09 +0300

    part2 start

--- FrameContainer.java
+++ FrameContainer.java
@@ -1,6 +1,6 @@
 import java.io.File;
 
-public class FrameContainer {
+public class FrameContainer implements ContainerFunctions   {
     int sizeOfArray;
     final int INITIAL_Size = 3;
     Frame[] FrameArray;
@@ -11,6 +11,48 @@ public class FrameContainer {
 
     public FrameContainer(File imageFile) {
 
+    }
+
+    @Override
+    public Frame get(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void add(Frame f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void remove(Frame f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void sort(Frame[] f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void rotateAll(Frame[] f) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void smoothAll(Frame[] f, int n) {
+        // TODO Auto-generated method stub
+        
     }
 
 }
